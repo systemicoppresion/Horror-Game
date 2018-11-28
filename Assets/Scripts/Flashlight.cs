14dcec376033d3420fa847612260dc01d80f21d8
SUSSEXDOWNS\474104 <474104@chrome.sussexdowns.ac.uk>
2018-11-28 12:21:56 +0000

    Coded a toggleable flashlight

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light flashlight;
+
+    private bool isActive;
+	// Use this for initialization
+	void Start ()
+    {
+        isActive = true;	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (isActive == false)
+            {
+                flashlight.enabled = true;
+                isActive = true;
+            }
+            else if (isActive == true)
+            {
+                flashlight.enabled = false;
+                isActive = false;
+            }
+        }
+	}
+}
