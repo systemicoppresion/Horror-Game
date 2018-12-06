0ceafe9ccc9287411fa5f64006331493894c4578
SUSSEXDOWNS\474104 <474104@chrome.sussexdowns.ac.uk>
2018-12-06 14:30:34 +0000

    Started working on door animations.

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("Door_open");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+}
